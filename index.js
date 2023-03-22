98ecca69d7b8dbed8c45342a5bac8c3e3e2ccbae
olawale-o <omoogunolawale@gmail.com>
2023-03-22 15:52:39 +0100

    Import slack app

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-const http = require('http');
 const config = require('./config');
-const app = require('./src/app');
+const app = require('./src/app')(config);
 
-
-const server = http.createServer(app);
-
-server.listen(config.get('port'), () => {
-  console.log(`Server is running on port ${config.get('port')}`);
+app.message('hello', async ({ message, say }) => {
+  console.log({
+    message,
+  });
+  await say({
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Hey there <@${message.user}>!`,
+        },
+        accessory: {
+          type: 'button',
+          text: {
+            type: 'plain_text',
+            text: 'Click Me',
+          },
+          action_id: 'button_click',
+        },
+      }
+    ],
+    text: `Hey there <@${message.user}>!`,
+  });
 });
 
-module.exports = server;
+app.action('button_click', async ({ body, ack, say }) => {
+  console.log({
+    body,
+    user: body.user,
+  })
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
+
+(async () => {
+  // Start your app
+  await app.start();
+
+  console.log(`⚡️ Bolt app is running! on port ${config.get('port')}`);
+})();
+
