31aa41ffef43bef0d1ccfc4286836d2f917558e1
olawale-o <omoogunolawale@gmail.com>
2023-03-24 13:40:55 +0100

    Use slack client

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const moment = require('moment');
+const { WebClient, LogLevel } = require("@slack/web-api");
 const config = require('./config');
 const app = require('./src/app')(config);
+const client = new WebClient(config.get('slack.bot_token'), {
+  logLevel: LogLevel.DEBUG,
+});
+
 
 app.message('hello', require('./src/messages/hello.message'));
 
@@ -9,10 +15,15 @@ app.event('member_joined_channel', require('./src/events/subscribers/member_join
 
 app.event('member_left_channel', require('./src/events/subscribers/member_left.event'));
 
-(async () => {
-  // Start your app
-  await app.start();
 
+(async () => {
+  await app.start();
+  require('./src/messages/post.message')({
+    client,
+    postAt: moment().add(5, 'minutes').unix(),
+    channelId: 'C04AQGNQE4S',
+    text: "Good day everyone @<channel>; please remember to send in your updates for the week. Thank you",
+  });
   console.log(`⚡️ Bolt app is running! on port ${config.get('port')}`);
 })();
 
